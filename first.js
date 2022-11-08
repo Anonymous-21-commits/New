db5b14991f12f1c03f5b7b440f26b133d7265711
Soumalya Majumdar <soumalyamajumdar18@gmail.com>
2022-11-09 02:32:40 +0530

    done

--- first.js
+++ first.js
@@ -4,3 +4,4 @@ console.log("made change from git");
 console.log("made second change from git");
 console.log("made second change from github");
 console.log("last change from github");
+console.log("made last change from git without getting merge conflict as had already pulled changes");
